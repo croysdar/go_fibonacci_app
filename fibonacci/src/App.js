c157c95fb96533fbfab7cdc0a7287e2394ca8124
Rebecca Croysdale <rebecca@croysdale.net>
2020-07-21 15:32:58 -0700

    Adds react app content and css

--- fibonacci/src/App.js
+++ fibonacci/src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import {Card, InputGroup, FormGroup, Button, Text} from "@blueprintjs/core"
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  state = {
+    amount: 3,
+    sequence: "",
+  }
+
+  getSequence = async (e) => {
+    e.preventDefault();
+    
+    fetch('api/fibonacci/' + this.state.amount)
+    .then(response => {
+      return response.text()
+    })
+    .then(text => {
+      console.log(text)
+      this.setState({sequence: text})
+
+    })
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <div className="App-body">
+          <Card className="App-card">
+            <h3>Fibonacci Sequence Generator</h3>
+            <h4>Enter a number to see a fibonacci sequence</h4>
+            <FormGroup >
+              <InputGroup 
+                id="text-input" 
+                value={this.state.amount}
+                onChange = {e =>  this.setState({amount: e.target.value}) }
+              />
+              <Button
+                type = "submit"
+                id = "submit-button"
+                text = "Get sequence"
+                onClick = { (e) => this.getSequence(e) }
+              />
+            </FormGroup>
+            <Text> <h5> {this.state.sequence} </h5> </Text>
+          </Card>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
